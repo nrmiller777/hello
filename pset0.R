7fd7393703896df1991ef60f2670c1b0111bf2ee
nrmiller777 <91348524+nrmiller777@users.noreply.github.com>
2021-09-24 14:05:02 -0700

    Update pset0.R
    I added a few more lines of code.

--- pset0.R
+++ pset0.R
@@ -1,1 +1,3 @@
 print("hello world")
+print("I tried to think of something clever to add to this code, but it didn't work.")
+print("This is a lesson learned the hard way: eight hours of sleep are useful for humor and creativity.")
